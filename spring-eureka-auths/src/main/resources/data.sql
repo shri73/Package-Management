2764fb4ab297675774fb0db6c245e1b95e324525
ss14527 <shriya.singhania@carleton.ca>
2020-08-03 23:12:09 -0400

    delete package and auth service

--- spring-eureka-auths/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-REPLACE INTO `stores` (store_no) VALUES ('S111');
-REPLACE INTO `stores` (store_no) VALUES ('S112');
-REPLACE INTO `stores` (store_no) VALUES ('S113');
-REPLACE INTO `stores` (store_no) VALUES ('S114');
-
-REPLACE INTO `beacons` (uuid, major, minor, store_no) VALUES ('1', '1', '1', 'S111');
-REPLACE INTO `beacons` (uuid, major, minor, store_no) VALUES ('1', '2', '2', 'S112');
-REPLACE INTO `beacons` (uuid, major, minor, store_no) VALUES ('1', '2', '3', 'S113');
-
