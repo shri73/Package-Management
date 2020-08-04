0360709a5a6d35e4fde43a428d1e3c3b3fe7d2f7
Shriya  Singhania <shriya.singhania@mavs.uta.edu>
2020-08-04 00:11:31 -0400

    remove unnecessary comments

--- spring-eureka-zuul/src/main/java/com/eureka/zuul/security/JwtAuthenticationEntryPoint.java
+++ spring-eureka-zuul/src/main/java/com/eureka/zuul/security/JwtAuthenticationEntryPoint.java
@@ -14,9 +14,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UNAUTHORIZED");
-        
-        // How to customize the retured message?
-        // Link: https://stackoverflow.com/a/40791087
+       
         String json = String.format("{\"message\": \"%s\"}", e.getMessage());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
